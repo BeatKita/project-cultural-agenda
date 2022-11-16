8f3dd8e17b5d1a1bff43726da44b63092b57d859
Pierrick <mazzella.pierrick@gmail.com>
2022-11-17 00:14:09 +0100

    modify events_controller.rb and new.html.erb

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,8 +8,13 @@ class EventsController < ApplicationController
   end
 
   def create
-    Event.create(event_params)
-    redirect_to events_path
+    @event = Event.new(event_params)
+
+    if @event.save
+      redirect_to events_path
+    else
+      render :new
+    end
   end
 
   private
